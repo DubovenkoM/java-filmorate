3de58b210115d596f8393686bedfa40a481d89e3
Mikhail_Dubovenko <dubovenko.m@yandex.ru>
2023-01-29 15:33:33 +0300

    refactor: sprint-9. 2 комит. использовать FieldDefautlts вместо множественных private

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,21 +1,23 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.*;
+import lombok.experimental.FieldDefaults;
 import ru.yandex.practicum.filmorate.validator.MovieBirthday;
 import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Data
 @Builder
+@FieldDefaults(level=AccessLevel.PRIVATE)
 public class Film {
     final String FIRST_FILM_RELEASE = "1895-12-28";
-    private Long id;
+    Long id;
     @NotBlank(message = "Название не может быть пустым")
-    private String name;
+    String name;
     @Size(max = 200, message = "Максимальная длина более 200 символов")
-    private String description;
+    String description;
     @MovieBirthday(value = FIRST_FILM_RELEASE)
-    private LocalDate releaseDate;
+    LocalDate releaseDate;
     @Min(value = 1, message = "Продолжительность фильма менее нуля")
-    private long duration;
+    long duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,15 +1,17 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.*;
+import lombok.experimental.FieldDefaults;
 import java.time.LocalDate;
 
 
 @Data
 @Builder
+@FieldDefaults(level=AccessLevel.PRIVATE)
 public class User {
-    private Long id;
-    private String email;
-    private String login;
-    private String name;
-    private LocalDate birthday;
+    Long id;
+    String email;
+    String login;
+    String name;
+    LocalDate birthday;
 }
